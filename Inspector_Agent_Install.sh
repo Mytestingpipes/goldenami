66a523804fc5a6f9bd9291a50a6e0e20d8343636
Mytestingpipes <121104790+Mytestingpipes@users.noreply.github.com>
2023-03-02 17:34:25 +0530

    Create Inspector_Agent_Install.sh

---
+++ Inspector_Agent_Install.sh
@@ -0,0 +1,5 @@
+echo "*******Installing AWS Inspector Agent********"
+
+wget https://inspector-agent.amazonaws.com/linux/latest/install
+curl -O https://inspector-agent.amazonaws.com/linux/latest/install
+sudo bash install
