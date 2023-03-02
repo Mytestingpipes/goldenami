840a1a4b499e4aea27471cc2d95990d6d59d2695
Mytestingpipes <121104790+Mytestingpipes@users.noreply.github.com>
2023-03-02 17:38:18 +0530

    Create variables.tf

---
+++ module2/variables.tf
@@ -0,0 +1,26 @@
+variable "subnet_id" {}
+#variable "instance_type" {}
+variable "availability_zone" {}
+#variable "environment" {}
+#variable "ami" {}
+
+
+variable region {
+    description = "AWS Region"
+    default = "us-east-1"
+}
+
+variable "stack" {
+    description = "Name of the Stack"
+    default = "Goldenami-v2"
+}
+
+variable "instances" {
+     default = 1
+     description = "No of instances"
+}
+
+variable "instance_type" {
+    description = "Size of the VM"
+    default = "t2.micro"
+}
