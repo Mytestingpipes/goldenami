c6d9aacf250f97a7dd21a1ecb1bdb5c5efb89eb4
Mytestingpipes <121104790+Mytestingpipes@users.noreply.github.com>
2024-03-07 15:44:02 +0530

    Update main.tf

--- module2/main.tf
+++ module2/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "ec2" {
   ami           = "${data.aws_ami.packer_image.id}"
   instance_type = "${var.instance_type}"
   subnet_id = "${var.subnet_id}"
+  iam_instance_profile = "SSM-role"
   tags = {
     Name = "${var.stack}"
   }
